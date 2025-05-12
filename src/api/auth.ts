1c0c38e2a5cb2904cf72eea5a1ff4f428168865e
elinaolfat <elinaolfat@gmail.com>
2025-05-12 01:41:50 -0700

    minor fixes in auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { User } from "../types";
 
-const API_BASE_URL = "http://localhost:8080/api/auth"; // Update with your backend URL
+const API_BASE_URL = "http://127.0.0.1:8080/api/auth"; // Update with your backend URL
 
 export const login = async (username: string, password: string): Promise<User> => {
-  const response = await axios.post("http://localhost:8080/login", new URLSearchParams({ username, password }), { withCredentials: true });
+  const response = await axios.post("http://127.0.0.1:8080/login", new URLSearchParams({ username, password }), { withCredentials: true });
   return response.data;
 };
 
